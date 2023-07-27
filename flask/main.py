323800aaa84711da53d41fb70a4d8194ed9d2f30
otaviootavio <otaviovacari@usp.br>
2023-07-27 14:47:17 -0300

    add temperature graph

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template
 import firebase_admin
 from firebase_admin import credentials, firestore
+from datetime import datetime, timedelta
+import plotly.graph_objects as go
+import plotly.offline as pyo
 import os
 
 # Use a service account.
@@ -32,10 +35,49 @@ def list_data():
             data.append({device_id: doc.to_dict()})
         else:
             print(f"No such document {device_id}!")
+    
+    plots = temperature_plot()
+    return render_template("list.html", data=data, plots = plots)
 
-    return render_template("list.html", data=data)
 
+def temperature_plot():
+    # Get current time and 24 hours ago
+    now = datetime.now()
+    one_day_ago = now - timedelta(hours=24)
 
+    # Fetch all document IDs in the 'devices' collection
+    device_ids = [doc.id for doc in db.collection('devices').stream()]
+
+    plots = {}
+
+    # Fetch data from each device
+    for device_id in device_ids:
+        # Get all log entries from the past 24 hours
+        logs = (db.collection('devices')
+                  .document(device_id)
+                  .collection('logs')
+                  .where('timestamp', '>', one_day_ago)
+                  .stream())
+
+        data = [log.to_dict() for log in logs]
+
+        # Sort data by timestamp
+        data.sort(key=lambda x: x['timestamp'])
+
+        # Extract temperature and timestamp data
+        timestamps = [x['timestamp'].strftime('%Y-%m-%d %H:%M:%S') for x in data]
+        temperatures = [x['temperature'] for x in data]
+
+        # Create plotly figure
+        fig = go.Figure(data=go.Scatter(x=timestamps, y=temperatures))
+
+        # Convert figure to HTML div string
+        plot_div = pyo.plot(fig, output_type='div')
+
+        # Map the device_id to the plot_div
+        plots[device_id] = plot_div
+
+    return plots
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
