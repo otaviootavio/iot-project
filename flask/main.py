8de4f2f55d44164d340b5c2ab2b82334423e264c
otaviootavio <otaviovacari@usp.br>
2023-07-27 14:27:51 -0300

    update view

--- flask/main.py
+++ flask/main.py
@@ -4,29 +4,38 @@ from firebase_admin import credentials, firestore
 import os
 
 # Use a service account.
-cred = credentials.Certificate('serviceAccount.json')
+cred = credentials.Certificate("serviceAccount.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/add', methods=['GET', 'POST'])
-def add_data():
-    if request.method == 'POST':
-        data = {i: request.form[i] for i in request.form}
-        db.collection('data').add(data)
-    return render_template('form.html')
 
-@app.route('/list')
+@app.route("/list")
 def list_data():
-    docs = db.collection('data').stream()
-    data = [{doc.id: doc.to_dict()} for doc in docs]
-    return render_template('list.html', data=data)
+    # Fetch all document IDs in the 'devices' collection
+    device_ids = [doc.id for doc in db.collection("devices").stream()]
+
+    data = []
+
+    # Fetch data from each device
+    for device_id in device_ids:
+        doc = db.collection("devices").document(device_id).get()
+        if doc.exists:
+            # Append a dict containing the device ID and its data
+            data.append({device_id: doc.to_dict()})
+        else:
+            print(f"No such document {device_id}!")
+
+    return render_template("list.html", data=data)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
